b42deacd5dbdd050b6ff68515d2124fdbd98d653
Will Haberer <willhaberer@gmail.com>
2022-02-03 10:23:31 -0500

    app can now take in a single word command from command line

--- multiclipboard.py
+++ multiclipboard.py
@@ -2,5 +2,14 @@ import sys
 import clipboard
 import json
 
-data=clipboard.paste()
-print(data)
+
+if len(sys.argv)==2:
+    command =sys.argv[1]
+    if command =="save":
+        print("save")
+    elif command == "load":
+        print("load")
+    elif command == "list":
+        print("list")
+    else:
+        print("Unknown Command")
