3a95529254598195aeb62ed0746f1fb2227402fc
Will Haberer <willhaberer@gmail.com>
2022-02-03 10:45:24 -0500

    defined the functionality for the save command

--- multiclipboard.py
+++ multiclipboard.py
@@ -2,19 +2,24 @@ import sys
 import clipboard
 import json
 
-def save_items(filepath, data):
+SAVED_DATA= "clipboard.json"
+
+def save_data(filepath, data):
         with open(filepath, "w") as f:
             json.dump(data, f)
 
-def load_items(filepath):
+def load_data(filepath):
     with open(filepath, "r")as f:
             data=json.load(f)
             return data
 
 if len(sys.argv)==2:
     command =sys.argv[1]
+    data=load_data(SAVED_DATA)
     if command =="save":
-        print("save")
+        key=input("Enter a key: ")
+        data[key]=clipboard.paste()
+        save_data(SAVED_DATA, data)
     elif command == "load":
         print("load")
     elif command == "list":
